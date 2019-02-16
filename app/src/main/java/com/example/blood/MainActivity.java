e6bd8103a2af2aaade1fe220c68aaafbf156e4cd
akashkumar2k10 <akashkumar2k10@gmail.com>
2019-02-16 19:09:19 +0530

    First page front page is completed

--- app/src/main/java/com/example/blood/MainActivity.java
+++ app/src/main/java/com/example/blood/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     EditText Editname;
     EditText EditPass;
     Button btnSubmit;
+    TextView AlreayUser;
+    TextView Blood_bank;
 
 
 
@@ -26,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
 
         Editname=(EditText)findViewById(R.id.editText);
         EditPass=(EditText)findViewById(R.id.editText2);
+        btnSubmit=(Button)findViewById(R.id.button);
+        AlreayUser=(TextView)findViewById(R.id.textView4);
+        Blood_bank=(TextView)findViewById(R.id.textView5);
+
+        AlreayUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"yo!!!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Blood_bank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"blood bank",Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }

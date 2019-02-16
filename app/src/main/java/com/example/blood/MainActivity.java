165670e83cad5e6ebab635723e079fbb790c5264
Ujjawal Dewangan <46930697+ujjujdp@users.noreply.github.com>
2019-02-16 19:15:14 +0530

    sign form

--- app/src/main/java/com/example/blood/MainActivity.java
+++ app/src/main/java/com/example/blood/MainActivity.java
@@ -3,30 +3,12 @@ package com.example.blood;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText Editname;
-    EditText EditPass;
-    Button btnSubmit;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Editname=(EditText)findViewById(R.id.editText);
-        EditPass=(EditText)findViewById(R.id.editText2);
-
-
     }
 }
